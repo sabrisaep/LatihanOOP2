2ffcf9db9edbfcdb90d4216ccd2fa3b994899bb8
sabrisaep <sabrisaep@gmail.com>
2021-04-28 13:12:32 +0800

    cuba cmd kali ke 2

--- LatihanOOP/Pelajar.vb
+++ LatihanOOP/Pelajar.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Pelajar
+    Private strNoMatrik As String
+
+    Sub New(ByVal strNoMatrik As String)
+        Me.strNoMatrik = strNoMatrik
+    End Sub
+
+    Public Function Politeknik() As String
+        Dim strKodPoli As String = strNoMatrik.Substring(0, 2)
+        If strKodPoli = "18" Then
+            Return "Politeknik Tuanku Syed Sirajuddin"
+        Else
+            Return "Bukan PTSS"
+        End If
+    End Function
+
 
 End Class
